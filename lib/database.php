4fd83f5d12ef3a04776053316e3d9e54df37d8aa
Quan <manhquan24072000@gmail.com>
2021-07-14 14:51:54 +0700

    commit code4

--- lib/database.php
+++ lib/database.php
@@ -1,6 +1,13 @@
 <?php 
     $filepath = realpath(dirname(__FILE__));
     include ($filepath.'/../config/config.php'); ?>
+<?php
+
+define('DB_HOST', '52.6.114.59');
+define('DB_USER', 'user');
+define('DB_PASS', '123');
+define('DB_NAME', 'web_ATN');
+?>
 
 <?php
 Class Database extends PDO{
