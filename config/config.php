29eec9e080acff2b58a56b791e0b3938cae6ff91
Quan <manhquan24072000@gmail.com>
2021-07-14 15:36:31 +0700

    commit code4

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_HOST', 'mysql-38481-0.cloudclusters.net');
-define('DB_USER', 'admin');
-define('DB_PASS', 'kUlWQOFe');
+define('DB_USER', 'quan');
+define('DB_PASS', 'quannd24');
 define('DB_NAME', 'ATN');
 ?>
