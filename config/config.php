6f0ce69771b1bcaa9f1b05f307dcd653b6ffd9f1
Quan <manhquan24072000@gmail.com>
2021-07-14 14:11:37 +0700

    commit code

--- config/config.php
+++ config/config.php
@@ -1,8 +1,5 @@
 <?php
-// define("DB_HOST", 'localhost');
-// define("DB_USER", 'root');
-// define("DB_PASS", '');
-// define("DB_NAME", 'web_mvcphu');
+
 define("DB_HOST", '52.6.114.59');
 define("DB_USER", 'atn-user');
 define("DB_PASS", '123');
