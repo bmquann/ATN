8c2724908f8f8ecb400e85a08ed09b9d245c88c2
Quan <manhquan24072000@gmail.com>
2021-07-14 15:43:24 +0700

    commit code4

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define("DB_HOST", '154.16.171.88');
+define("DB_HOST", '52.6.114.59');
 define("DB_USER", 'quan');
-define("DB_PASS", 'quannd24');
+define("DB_PASS", '123@123a');
 define("DB_NAME", 'ATN');
 ?>
